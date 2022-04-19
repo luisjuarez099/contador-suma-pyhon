58032bb22bda8e14835c09b379d40127b8eccbe2
LuisJuarez@gmail.com <LuisJuarez@gmail.com>
2022-04-18 20:29:29 -0500

    Actualizamos una funcion de suma introducida por el usuario y el nombre del usuario

--- gradosFC.py
+++ gradosFC.py
@@ -4,11 +4,19 @@ from unittest import result
 def sumatoria(a,b):
     result=a+b
     print(result)
-    return result
-sumatoria(21,34)
+a=int(input("Digita un numero: "))
+b=int(input("Digita un numero: "))
+
+
 
 def name(name):
     return name
 name=input("Digita tu nombre: ")
 print(f"Hola {name}")
 
+def promedio():
+    pass
+
+if __name__=='__main__':
+    sumatoria(a,b)
+    name

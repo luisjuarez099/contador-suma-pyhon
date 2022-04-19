23ec44197826e6d6717d80e06b02546aed8715e7
LuisJuarez@gmail.com <LuisJuarez@gmail.com>
2022-04-18 20:48:09 -0500

    comentamos suma y name. Agregamos una sumatoria con bucle while que ingresa numeros por usuario

--- gradosFC.py
+++ gradosFC.py
@@ -1,22 +1,29 @@
-from unittest import result
-
-
-def sumatoria(a,b):
-    result=a+b
-    print(result)
-a=int(input("Digita un numero: "))
-b=int(input("Digita un numero: "))
+# def sumatoria(a,b):
+#     result=a+b
+#     print(result)
+# a=int(input("Digita un numero: "))
+# b=int(input("Digita un numero: "))
 
 
 
-def name(name):
-    return name
-name=input("Digita tu nombre: ")
-print(f"Hola {name}")
+# def name(name):
+#     return name
+# name=input("Digita tu nombre: ")
+# print(f"Hola {name}")
 
 def promedio():
-    pass
+    print("Conoceremos el promedio")
+    dato=0
+    i=1
+    while i<=4:
+        num=int(input(f"Digita un numero {i}: "))
+        dato=num+dato
+        print(dato)
+        i+=1
+    print(f"El resultado de sus numeros agregados es de {dato}")
+        
 
-if __name__=='__main__':
-    sumatoria(a,b)
-    name
+if __name__== '__main__':
+    # sumatoria(a,b)
+    # name
+    promedio()

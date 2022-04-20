8e8e365b192f5980d49112ef0c3e6cd329e48306
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-04-20 18:02:27 -0500

    borre un print(dato)

--- gradosFC.py
+++ gradosFC.py
@@ -1,15 +1,3 @@
-# def sumatoria(a,b):
-#     result=a+b
-#     print(result)
-# a=int(input("Digita un numero: "))
-# b=int(input("Digita un numero: "))
-
-
-
-# def name(name):
-#     return name
-# name=input("Digita tu nombre: ")
-# print(f"Hola {name}")
 
 def promedio():
     print("Conoceremos el promedio")
@@ -18,12 +6,9 @@ def promedio():
     while i<=4:
         num=int(input(f"Digita un numero {i}: "))
         dato=num+dato
-        print(dato)
         i+=1
     print(f"El resultado de sus numeros agregados es de {dato}")
 if __name__== '__main__':
-    # sumatoria(a,b)
-    # name
     promedio()
 
     
